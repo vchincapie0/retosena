45bb068f543fc6d8389ff858a3d778b03b5df5b5
vivian_pc <carohinca1997@gmail.com>
2023-09-15 16:00:40 -0500

    template provisional login, validacion de usuarios

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect,url_for,flash
+from flask import Flask, render_template, request, redirect,url_for,flash,session
 from flask_mysqldb import MySQL 
 
 app = Flask(__name__)#Se especifica que este archivo es el que va a iniciar la webapp
@@ -48,14 +48,29 @@ def registro():
         cur.execute('INSERT INTO ciudadanos(tipoDocumento,numeroDocumento,nombres,apellidos,sexo,fechaNacimiento,telefonoCelular,telefonoFijo,nivelEducativo,conectividad,etnia,dicapacidad,dispositivosTecnologicos,cualesDispTecn,regimenAfiliacion,municipio,barrioVereda,direccion,estrato,email,contraseña) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)',(tipodocumento,documento,nombres,apellidos,sexo,fechanacimiento,telefonoCelular,telefonoFijo,educacion,conectividad,etnia,discapacidad,accesoTecnologia,cualAccTecnol,regimen,municipio,barrioVereda,direccion,estrato,email,password))
         mysql.connection.commit()
         flash('Ciudadano Agregado')
-        return redirect(url_for('index'))
+        return redirect(url_for('index'))#Una vez el ciudadano este registrado se redirecciona al index
     return render_template('registro.html')
 
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET','POST'])
 def login():
     '''Funcion para el ingreso de usuarios'''
+    if request.method == 'POST' and 'email' in request.form and 'contrasenia':
+        email=request.form['email']
+        password=request.form['contrasenia']
+
+        cur = mysql.connection.cursor()
+        cur.execute('SELECT * FROM ciudadanos where email=%s AND contraseña=%s',(email,password,))
+        account = cur.fetchone()
+
+        if account:
+            session['Logueado']=True
+
+            return redirect(url_for('index'))
+        else:
+            flash('Usuario y/o contraseña incorrecta')
+            return render_template('login.html')
     return render_template('login.html')
 
 if __name__=='__main__':
